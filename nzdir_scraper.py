ef08fb7cffb807f29790b6b553d01a14ec121602
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-03-04 00:21:22 +1200

    Update nzdir_scraper.py
    scraper can now grab the name, address, and phone from the lead string. It now stores leads in a multidimensional list

--- nzdir_scraper.py
+++ nzdir_scraper.py
@@ -19,7 +19,31 @@ def scrape(category = ''):
     total_pages = len(soup.find_all('div', {'class': 'pages'}))
     current_page = 1
     while current_page <= total_pages:
-        leads.extend(soup.find_all('p', {'class': 'address'}))
+        for i in range(len(soup.find_all('p', {'class': 'address'}))):
+            lead = soup.find_all('p', {'class': 'address'})[i].text
+
+            #get individual items from lead string
+            try:
+                name = lead[0: lead.index(',')]
+                try:
+                    address = lead[lead.index(',') + 2: lead.index('+') - 1]
+                except:
+                    try:
+                        address = lead[lead.index(',') + 2: lead.index('(') - 1]
+                    except:
+                        continue
+                try:
+                    telephone = lead[lead.index('+'): len(lead)]
+                except:
+                    try:
+                        telephone = lead[lead.index('('): len(lead)]
+                    except:
+                        continue
+            except:
+                continue
+
+            leads.extend([[name, address, telephone]])
+
         current_page += 1
 
         #send request and gather src code
@@ -27,9 +51,9 @@ def scrape(category = ''):
         result = requests.get(full_url)
         src = result.content
         soup = BeautifulSoup(src, 'html.parser')
-
+    
     for lead in leads:
-        print(lead.text)
+        print(lead)
 
 
 
