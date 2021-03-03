fd5cff4ec8af2d2aeef806078d6c3146c4bff4e9
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-03-03 21:07:46 +1200

    Update nzdir_scraper.py
    put stable version of scraper into a function

--- nzdir_scraper.py
+++ nzdir_scraper.py
@@ -8,13 +8,26 @@ from bs4 import BeautifulSoup
 def scrape(category = ''):
     BASE_URL = 'https://www.nzdirectory.co.nz/'
 
+    #send request and gather src code
     full_url = BASE_URL + category + '.html'
     result = requests.get(full_url)
     src = result.content
     soup = BeautifulSoup(src, 'html.parser')
 
     leads = []
-    leads = soup.find_all("p", {"class": "address"})
+
+    total_pages = len(soup.find_all('div', {'class': 'pages'}))
+    current_page = 1
+    while current_page <= total_pages:
+        leads.extend(soup.find_all('p', {'class': 'address'}))
+        current_page += 1
+
+        #send request and gather src code
+        full_url = BASE_URL + category + '-' + str(current_page) + '.html'
+        result = requests.get(full_url)
+        src = result.content
+        soup = BeautifulSoup(src, 'html.parser')
+
     for lead in leads:
         print(lead.text)
 
