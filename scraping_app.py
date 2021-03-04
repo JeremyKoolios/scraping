f54826c4930e6826e83fc6c7a724e0a645a34814
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-03-05 11:18:24 +1200

    employed the use of jinja2 module to create a table of all leads

--- scraping_app.py
+++ scraping_app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+from jinja2 import Template
 import nzdir_scraper as scraper
 
 scraping_app = Flask(__name__)
 
 @scraping_app.route('/')
 def index():
-    return render_template('display_results.html')
+    return render_template('display_results.html', leads = scraper.scrape('automotives'))
